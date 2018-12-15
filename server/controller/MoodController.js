bdb6817984989edef1dff8321c0896d6dad2d5ae
Jens Ottmann <jottmann@nanocosmos.de>
2018-12-15 23:05:31 +0100

    add basic mood controller

--- server/controller/MoodController.js
+++ server/controller/MoodController.js
@@ -4,6 +4,14 @@ class MoodController{
   static getMoods(){
     return {};
   }
+
+  static getMood(){
+    return {};
+  }
+
+  static addMood(mood){
+    return Promise.resolve(mood);
+  }
 }
 
 module.exports = MoodController;
