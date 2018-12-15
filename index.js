636d3b08b21f53465fc291a881e4a364e3f19997
Jens Ottmann <jottmann@nanocosmos.de>
2018-12-15 23:05:57 +0100

    use new controllers for the first requests

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 "use strict";
 
-const express = require('express');
 
+const express = require('express');
 const app = express();
+
+const bodyparser = require('body-parser');
+
+const QuestionsController = require('./server/controller/QuestionController');
+const AnswerController = require('./server/controller/AnswerController');
+const MoodInquiryController = require('./server/controller/MoodInquiryController');
+const MoodPictureController = require('./server/controller/MoodPictureController');
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+
 
 app.get('/', function (req, res) {
   return res.send('emoto is running');
@@ -17,15 +33,32 @@ app.get('/moods', function (req, res) {
 });
 
 app.post('/mood', function (req, res) {
-  return res.status(501);
+  console.log(req.body);
+  return MoodPictureController.createMoodPicture(req.body)
+  .then(function(m){
+    return res.send(m);
+  });
 });
 
 app.get('/questions', function(req, res){
-  return res.status(501);
+  return QuestionsController.getAllPublic()
+  .then(function(questions){
+    return res.send(questions);
+  });
 });
 
 app.get('/answers', function(req, res){
-  return res.status(501);
+  return AnswerController.getAll()
+  .then(function(answers){
+    return res.send(answers);
+  });
+});
+
+app.get('/moodInquiry', function(req, res){
+  return MoodInquiryController.getBasicMoodInquiry()
+  .then(function(moodInquiry){
+    return res.send(moodInquiry);
+  });
 });
 
 app.get('/crewmood', function (req, res) {
