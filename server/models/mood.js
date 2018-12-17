eb314f0c17d2bf744fadb9cc7f67b1ae1e4c67ce
Jens Ottmann <jottmann@nanocosmos.de>
2018-12-17 17:50:24 +0100

    add comment field to mood

--- server/models/mood.js
+++ server/models/mood.js
@@ -1,13 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  //ToDo Refactore: Foreign Key Fields not necessary at this point
+  //But in this case, the inser/create statement will be different
+  //A cleanup and refactoring will be useful
   const Mood = sequelize.define('Mood', {
     moodPictureId: DataTypes.INTEGER,
     questionId: DataTypes.INTEGER,
-    answerId: DataTypes.INTEGER
-    //ToDo: Add note field
+    answerId: DataTypes.INTEGER,
+    comment:DataTypes.STRING
   }, {});
   Mood.associate = function(models) {
-    Mood.belongsTo(models.User);
     Mood.belongsTo(models.Question);
     Mood.belongsTo(models.Answer);
     Mood.belongsTo(models.MoodPicture);
