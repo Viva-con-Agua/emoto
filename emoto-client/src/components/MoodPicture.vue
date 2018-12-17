6742dfa4428168252ef3f98592535eb317d33b78
Jens Ottmann <jottmann@nanocosmos.de>
2018-12-17 18:02:40 +0100

    add basic implementation for mood picture component

---
+++ emoto-client/src/components/MoodPicture.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="MoodInquiry">
+    {{moodPicutre}}
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import Vue from 'vue'
+
+export default {
+  name: 'MoodPicture',
+  data (){
+    return {
+      moodPicutre: null
+    }
+  },
+  created(){
+    this.getMoodPicutre
+  },
+  methods: {
+    getMoodPicutre: function(){
+      axios.get('http://localhost:3000/lastMoodPicture')
+      .then(response => {
+        switch(response.status){
+          case 200: 
+            this.moodPicutre = response.data
+            break
+        }
+      })
+      .catch(error => {
+                if(error.response){
+                  this.errorState = error.response.status
+                }
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
