de49ee92a1d83ee0d85b85c71a18f6debcb82835
Jens Ottmann <jottmann@nanocosmos.de>
2019-04-16 22:18:55 +0200

    textual adjustments

--- client/src/components/Info.vue
+++ client/src/components/Info.vue
@@ -6,13 +6,13 @@
         <span><strong>Wichtiger Hinweis</strong></span>
       </div>
       <div  class="text item">
-        Solltest du in deinem Ehrenamt frustriert sein oder sollte es andere Themen geben, bei denen du Unterstützung brauchst, wende dich an das Brunnenbüro oder die Ansprechperson für den Bereich Netzwerk in deiner Crew.
+        Solltest du in deinem Engagement frustriert sein oder sollte es andere Themen geben, bei denen du Unterstützung brauchst, wende dich an die <a href="mailto:netzwerk@vivaconagua.org">Geschäftsstelle</a> oder die Netzwerk-Ansprechperson deiner Crew.
         EMOTO ist lediglich für die persönliche Dokumentation deiner Stimmung da. 
       </div>
     </el-card>
     <p>
-      EMOTO ist ein Stimmungsbarometer, welches es dir ermöglicht deine Stimmung im Bezug auf dein Ehrenamt für VcA zu dokumentieren.
-      Durch die Dokumentation deiner Stimmung sollst du dazu motiviert werden, dein Ehrenamt aktiver zu reflektieren und bewusster wahrzunehmen. 
+      EMOTO ist ein Stimmungsbarometer, welches es dir ermöglicht deine Stimmung im Bezug auf dein Engagement für VcA zu dokumentieren.
+      Durch die Dokumentation deiner Stimmung sollst du dazu motiviert werden, dein Engagement aktiver zu reflektieren und bewusster wahrzunehmen. 
     </p>
     <p>
       Für die Abfrage deiner Stimmung gibt EMOTO drei Fragen vor. 
@@ -21,7 +21,8 @@
       Diese Fragen sind für dich und werden nicht den anderen Supporter*innen angezeigt.
     </p>
     <p>
-      Neben der Dokumentation deiner Stimmung zeigt dir EMOTO den Verlauf deiner Stimmung an. Hierdurch sollst du negative Entwicklungen früher erkennen, um bei Bedarf zu agieren.
+      Neben der Dokumentation deiner Stimmung zeigt dir EMOTO deren Verlauf an. Hierdurch kannst du erkennen, in welchen Phasen deines Engagements oder durch welche Aktionen du dich in deinem Potenzial befindest und welche Phasen/Aktionen weit entfernt von diesem sind.
+Damit kannst du für die Zukunft selbst entscheiden, welche Aktionen dir gut tun und in welchen Phasen du Motivation oder Pausen brauchst.
     </p>
     <p>
       EMOTO wurde im Rahmen mehrerer Forschungsarbeiten und in Zusammenarbeit mit mehreren Supporter*innen von VcA erarbeitet und befindet sich aktuell in einer ersten Testphase. Die Testphase wird durch eine Masterarbeit begleitet. 
